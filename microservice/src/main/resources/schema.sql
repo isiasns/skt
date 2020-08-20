8c1fe8e2676103fa65def33fcbc6dae539738cee
Isaac Sias <isias@nearsoft.com>
2020-08-20 17:55:58 -0600

    Added initial script for schema, connection properties and defined mysql version.

---
+++ microservice/src/main/resources/schema.sql
@@ -0,0 +1,32 @@
+drop table if exists skt.products;$$
+
+create table skt.products
+(
+    id          int auto_increment primary key,
+    sku         int          not null,
+    name        varchar(30)  not null,
+    description varchar(200) not null,
+    units       int          not null,
+    constraint products_name_uindex unique (name),
+    constraint products_sku_uindex unique (sku)
+);$$
+
+drop procedure if exists skt.get_all_products;$$
+
+create procedure skt.get_all_products()
+begin
+    select id,
+           sku,
+           name,
+           description,
+           units
+    from products;
+end;$$
+
+drop procedure if exists skt.insert_product;$$
+
+create procedure skt.insert_product(IN p_sku int, IN p_name varchar(30), IN p_description varchar(200), IN p_units int)
+begin
+    insert into products (sku, name, description, units)
+    values (p_sku, p_name, p_description, p_units);
+end;$$
--- microservice/src/test/java/com/skt/exercise/microservice/MicroserviceApplicationTests.java
+++ microservice/src/test/java/com/skt/exercise/microservice/MicroserviceApplicationTests.java
@@ -1,6 +1,6 @@
 package com.skt.exercise.microservice;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
