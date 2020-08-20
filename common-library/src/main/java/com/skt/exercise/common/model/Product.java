3806bcc4f96d9a86d9aa0f1cff01dcd160f36f9d
Isaac Sias <isias@nearsoft.com>
2020-08-19 20:54:02 -0600

    Added product model.

---
+++ common-library/src/main/java/com/skt/exercise/common/model/Product.java
@@ -0,0 +1,15 @@
+package com.skt.exercise.common.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+    private int id;
+    private String sku;
+    private String description;
+    private int units;
+}
