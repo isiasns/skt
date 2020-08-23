484347570a4dda03b603cf70355f99cddb58f994
Isaac Sias <isias@nearsoft.com>
2020-08-23 17:25:31 -0600

    Added dto mapper test.

--- microservice/src/test/java/com/skt/exercise/microservice/util/DtoMapperTest.java
+++ microservice/src/test/java/com/skt/exercise/microservice/util/DtoMapperTest.java
@@ -1,5 +1,17 @@
 package com.skt.exercise.microservice.util;
 
+import com.skt.exercise.common.model.Product;
+import com.skt.exercise.microservice.dto.ProductDto;
+import org.junit.Test;
+
+import static org.junit.Assert.assertNotNull;
+
 public class DtoMapperTest {
 
+    @Test
+    public void givenProductDtoWhenMapToPojoReturnProduct() {
+        ProductDto productDto = ProductDto.builder().build();
+        Product result = DtoMapper.mapToPojo(productDto);
+        assertNotNull(result);
+    }
 }
